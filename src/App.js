6e7771a15ae007b6ca6f4044b09f9e3afcb80188
PrachiGour2023 <prachi.gour602@gmail.com>
2023-04-10 14:54:04 +0530

    display contact list using api

--- src/App.js
+++ src/App.js
@@ -1,41 +1,11 @@
+import React from 'react';
 import './App.css';
 import ContactList from './contact/contactList';
 
 function App() {
-
-  const contacts = [
-    {
-      id: 1,
-      avatarUrl: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80",
-      name: "Prachi Gour",
-      email: "prachi@gmail.com",
-      city: "Indore"
-    },
-    {
-      id: 2,
-      avatarUrl: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80",
-      name: "Riya Sharma",
-      email: "riya@gmail.com",
-      city: "Pune"
-    },
-    {
-      id: 3,
-      avatarUrl: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80",
-      name: "Neha Pandey",
-      email: "neha@gmail.com",
-      city: "Kota"
-    },
-  ];
-
   return (
     <div className='App'>
-      {
-        contacts.map((contact, i) => {
-          return (
-            <ContactList key={i} avatarUrl={contact.avatarUrl} name={contact.name} email={contact.email} city={contact.city} />
-          )
-        })
-      }
+            <ContactList/>
     </div>
   )
 }
--- src/contact/contactList.js
+++ src/contact/contactList.js
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const ContactList = (props) => {
+const ContactList = () => {
+    const [contacts, setContacts] = useState([]);
+
+    useEffect(() => {
+        fetch("https://randomuser.me/api/?results=10")
+            .then(response => response.json())
+            .then(data => { setContacts(data.results) })
+            .catch(err => console.log(err))
+    }, []);
+
     return (
-        <div className='contact-card'>
-           <img src={props.avatarUrl} alt='profile'/>
-           <div className='user-details'>
-           <p><b>Name</b>: {props.name}</p>
-           <p><b>Email</b>: {props.email}</p>
-           <p><b>Location</b>: {props.city}</p>
-           </div>
-        </div>
+        <>
+            {
+                contacts.map((contact, i) => {
+                    return (
+                        <div className='contact-card' key={i}>
+                            <img src={contact?.picture?.medium} alt='profile' />
+                            <div className='user-details'>
+                                <p><b>Name</b>: {contact?.name?.first}{contact?.name?.last}</p>
+                                <p><b>Email</b>: {contact?.email}</p>
+                                <p><b>Age</b>: {contact?.dob?.age}</p>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </>
     )
 }
 
